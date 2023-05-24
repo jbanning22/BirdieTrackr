dd5c4088986de297f636ea79e67c2cf017f4d1e0
Jack Banning <jack1119banning@gmail.com>
2023-05-24 15:46:36 -0400

    test comment added in index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * @format
  */
 
-import {AppRegistry} from "react-native";
-import App from "./App";
-import {name as appName} from "./app.json";
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 // global.HermesInternal = {maxNumProperties: 250000};
+// test
