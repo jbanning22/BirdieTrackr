7f1871119c556a19363b19d6072b4419c0e3ccb7
Jack Banning <jack1119banning@gmail.com>
2023-06-06 12:16:46 -0400

    style changes applied to sign in screen

--- components/SignInScreen.test.js
+++ components/SignInScreen.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, screen, fireEvent} from '@testing-library/react-native';
+import {render, screen} from '@testing-library/react-native';
 import SignInScreen from './SignInScreen';
 import {AuthContext} from '../AuthContext';
 
